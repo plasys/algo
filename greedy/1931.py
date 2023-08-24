644a0c96757efcef86ef3f0a15cc5318be3a5373
Yoon Ki Kim <99261447+plasys@users.noreply.github.com>
2023-08-24 22:57:35 +0900

    Update 1931.py

--- greedy/1931.py
+++ greedy/1931.py
@@ -3,22 +3,18 @@ import sys
 n = int(sys.stdin.readline().rstrip())
 li = [tuple(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
 
-li.sort(key=lambda x:(x[0], x[1]))
+# 시작시간을 기준으로 먼저 정렬 후 종료시간을 기준으로 다시 정렬한다. 
+# 종료시간 오름차순으로 정렬되고, 시작시간이 같으면 역시 오름차순으로 정렬된다.
+li.sort(key=lambda x:x[0])
+li.sort(key=lambda x:x[1])
 
 # 시작점 
-left = li[0][0] # 강의 시작시간
 right = li[0][1] # 강의 종료시간
-
 cnt = 1
-for e in li[1:]:
-  # 강의 종료시간이 기존 종료시간보다 더 짧으면 시작점 변경
-  if e[1] < right:
-    left = e[0]
-    right = e[1]
-    
+
+for e in li[1:]: 
   # 강의 시작시간이 기존 종료시간보다 더 크면 다음 시작점이 되고 카운트를 올림
-  elif e[0] >= right:
-    left = e[0]
+  if e[0] >= right:
     right = e[1]
     cnt += 1
 
