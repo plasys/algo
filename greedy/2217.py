89f7d00a7378d15bd213ab0439b5a8f358a5454a
Yoon Ki Kim <99261447+plasys@users.noreply.github.com>
2023-09-04 23:29:38 +0900

    Update 2217.py

--- greedy/2217.py
+++ greedy/2217.py
@@ -1,1 +1,20 @@
-n = int(input())
+# n은 책의 갯수, m은 한 번에 들 수 있는 책의 갯수
+n, m = map(int, input().split())
+
+# 책들의 원래 위치 좌표
+li = list(map(int, input().split()))
+
+# 양수, 음수 좌표 나눠서 저장
+plusList = []
+minusList = []
+
+for i in li:
+  if i > 0:
+    plusList.append(i)
+  else:
+    minusList.append(i)
+
+d = []
+minusList.sort()
+
+# 1231231231231
