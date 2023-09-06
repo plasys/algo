a5890218f976a58a4ec5230ea0347a37b2eed3ae
Yoon Ki Kim <99261447+plasys@users.noreply.github.com>
2023-09-06 23:19:07 +0900

    Update 1141.py

--- greedy/1141.py
+++ greedy/1141.py
@@ -1,1 +1,32 @@
-n = int(input())
+n, m = map(int, input().split())
+
+li = list(map(int, input().split()))
+
+plusList = []
+minusList = []
+
+for i in li:
+  if i > 0:
+    plusList.append(i)
+  else:
+    minusList.append(i)
+
+d = []
+minusList.sort()
+
+for i in range(len(minusList) // m):
+  d.append(-minusList[m * i])
+  
+if len(minusList) % m != 0:
+  d.append(-minusList[(len(minusList) // m) * m])
+
+plusList.sort(reverse = True)
+for i in range(len(plusList) // m):
+  d.append(plusList[m * i])
+if len(plusList) % m != 0:
+  d.append(plusList[(len(plusList) // m) * m])
+
+d.sort()
+result = d.pop()
+result += 2 * sum(d)
+print(result)
